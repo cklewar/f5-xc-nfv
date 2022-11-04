4a9c95558e8860391efba5ef7c476f206360651b
cklewar@t-online.de <cklewar@t-online.de>
2022-11-04 12:19:32 +0100

    0.11.16 v2

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  f5xc_aws_tgw_name        = format("%s-%s-%s", var.project_prefix, var.f5xc_aws_tgw_name, var.project_suffix)
+  f5xc_aws_tgw_name = format("%s-%s-%s", var.project_prefix, var.f5xc_aws_tgw_name, var.project_suffix)
 }
