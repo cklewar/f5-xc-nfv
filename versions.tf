a71c0a27d445d9c9755d6e9a648ef74c3446b360
cklewar <cklewar@t-online.de>
2023-03-13 09:42:42 +0100

    Launch 0.11.19

--- versions.tf
+++ versions.tf
@@ -2,24 +2,23 @@ terraform {
   required_version = ">= 1.3.0"
   cloud {
     organization = "cklewar"
+    hostname     = "app.terraform.io"
 
     workspaces {
       name = "f5-xc-nfv-module"
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.14"
+      source = "volterraedge/volterra"
+      version = "= 0.11.19"
     }
-
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = ">= 4.32.0"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
