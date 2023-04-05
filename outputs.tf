0797d57e59cbfa142f5aa1cf491c13ab4660daa6
cklewar <c.klewar@f5.com>
2023-04-05 13:23:01 +0200

    0.11.20 v72

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "nfv" {
   value = {
-    nfv = module.nfv.nfv
+    //nfv = module.nfv.nfv
     tgw = module.tgw.f5xc_aws_tgw
   }
 }
 
-output "pan_commands" {
+/*output "pan_commands" {
   value = module.nfv.nfv["pan_commands"]
-}
+}*/
